c1b38615c9b3d94d5e23738d3531ec599670f8b8
elielnp <elielnp@outlook.com>
2023-04-10 10:04:27 -0300

    First commit

---
+++ Entities/Notifications/Notification.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Notifications
+{
+    public class Notification
+    {
+        public Notification() 
+        {
+            Notify = new List<Notification>();
+        }
+        public string PropoertyName { get; set; }
+
+        public string Message { get; set; }
+
+        public List<Notification> Notify;
+
+        public bool StringPropoertyValidation(string value, string propoertyName)
+        {
+            if (string.IsNullOrWhiteSpace(value) || string.IsNullOrWhiteSpace(propoertyName))
+            {
+                Notify.Add(new Notification
+                {
+                    Message = "Campo obrigatório",
+                    PropoertyName = propoertyName
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
