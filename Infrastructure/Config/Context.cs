378c20c9f06c26b9b5dd4720f76e8e51f3a8bf3a
elielnp <elielnp@outlook.com>
2023-04-14 22:31:41 -0300

    creating infrastructure db configs

---
+++ Infrastructure/Config/Context.cs
@@ -0,0 +1,30 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Config
+{
+    public class Context : IdentityDbContext<ApplicationUser>
+    {
+        public Context(DbContextOptions<Context> options) : base (options) 
+        {
+
+        }
+
+        public DbSet<News> News { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+        public string GetConectionString() 
+        {
+            string strcon = "";
+            return strcon; 
+        }
+    }
+
+}
